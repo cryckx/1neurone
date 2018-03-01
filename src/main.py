0dff26f5006fd4b183cc9ee66f9f42074c753e23
shashcode <shashcode@gmail.com>
2018-03-01 20:58:23 +0100

    input_read
    lecture des specs de l'entrée

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from inputhandler import *
-print os.path.dirname(os.path.realpath(__file__))
-print os.getcwd()
+
 entree = read_input("./example/a_example.in")
 
+rows = entree[0][0]
+columns = entree[0][1]
+vehicles = entree[0][2]
+rides = entree[0][3]
+bonus = entree[0][4]
+steps = entree[0][5]
+ride_list = entree[1:]
+
+
 print entree
+print ride_list
+print rows, columns, vehicles, rides, bonus, steps
