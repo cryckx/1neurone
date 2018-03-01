7d52e7fd7a35fd00af07da9c4d591ca95499d7db
shashcode <shashcode@gmail.com>
2018-03-01 22:08:41 +0100

    output
    fonction pour la sortie

--- PythonApplication1/PythonApplication1/iohandler.py
+++ PythonApplication1/PythonApplication1/iohandler.py
@@ -12,3 +12,13 @@ def read_input(file_path):
     splited = [e.split() for e in remove_line]
     int_entree = [map(int, e) for e in splited]
     return int_entree
+
+def write_output(file_path, output):
+    f = open(file_path, 'w')
+    for i in range(len(output)):
+        for j in range(len(output[i])):
+            f.write(str(output[i][j]))
+            f.write(' ')
+        f.write('\n')
+
+    f.close()
